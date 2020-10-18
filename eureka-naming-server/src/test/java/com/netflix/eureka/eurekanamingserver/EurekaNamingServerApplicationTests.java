e327513d0062db40167464212273559c14f58c6c
anubhav <anubhavgupta1012@gmail.com>
2020-10-18 13:24:58 -0700

    registered with Eureka Naming Server

---
+++ eureka-naming-server/src/test/java/com/netflix/eureka/eurekanamingserver/EurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.netflix.eureka.eurekanamingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
